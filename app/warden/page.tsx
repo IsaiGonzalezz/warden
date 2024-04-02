8072ae84e1a3884445cebfa2f842e9ae9b814b72
IsaiGonzalezz <juliangg.ti22@utsjr.edu.mx>
2024-04-02 00:44:40 -0600

    adding register - fix error

--- app/warden/page.tsx
+++ app/warden/page.tsx
@@ -1,1 +1,20 @@
 import React from "react";
+
+const Warden = () => {
+  return (
+    <div className="flex flex-col h-screen bg-gray-100">
+      <title>Warden</title>
+      <header className="p-4 bg-white shadow-md">
+        <div className="container mx-auto flex justify-between items-center">
+          <img
+            src="/img/banner.png"
+            alt="Warden Tech-Secure"
+            className="w-48"
+          />
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Warden;
